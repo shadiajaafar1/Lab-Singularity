908abaeb829aa19c248f1220e32814faeb361ac0
shadiajaafar1 <110212778+shadiajaafar1@users.noreply.github.com>
2022-11-21 23:19:11 -0500

    entrega ultima para siempre :)

--- main.py
+++
@@ -1,8 +0,0 @@
-from classes import DoubleLinkedList
-from classes import clean_df 
-
-lista_d = DoubleLinkedList()
-for i in range(len(clean_df.index[0:25])):
-    lista_d.AddNode(i, 'Departamento_ocurrencia', 'QUINDIO')
-    
-lista_d.Recorrido()
